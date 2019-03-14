d054cdb6c613e9b4613cec923ac324e2a5f03a16
brunosana <brunosana@outlook.com.br>
2019-03-14 18:04:32 -0300

    Ultimos arquivos de Array em JS

---
+++ Array/concat.js
@@ -0,0 +1,10 @@
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Washington', 'Wesclei']
+
+const todos = filhas.concat(filhos)
+
+console.log(todos)
+
+const todos2 = filhas.concat(filhos, 'Joel da Silva') //Concatena e adiciona elementos
+
+console.log(todos2)
